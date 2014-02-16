14efb8196c74d58ea8a97c6c99e1ea41e6789e59
tanaevr <tanaevr@gmail.com>
2014-02-16 21:10:22 +0400

    Add: code snippet

--- mychanks.snippet.php
+++ mychanks.snippet.php
@@ -1,3 +1,19 @@
 <?php
-
+	$output = '';
+	$error = '';
+	$chank_name = '';
+	$options = $modx->fromJSON($params);
+	$id = !empty($pageId) ? $pageId : $modx->resource->id;
+	$default_chank = !empty($default_chank) ? $default_chank : $error = 'Вы не указали $default_chank!';
+	$template_id = $modx->resource->template;
+	
+	if($options[$template_id]){
+		$chank_name = $options[$template_id];
+	}else{
+		$chank_name = $default_chank;
+	}
+	$output = $modx->getChunk($chank_name);
+	if(!empty($error)) $output = $error;
+	
+	return $output;
 ?>
